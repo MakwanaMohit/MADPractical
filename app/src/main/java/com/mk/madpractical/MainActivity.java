30871dd2b15874042d3b01618e0c82f04e02aed0
Mohit Makawana <makavanamohi777@gmail.com>
2024-08-09 14:58:52 +0530

    Initial commit

---
+++ app/src/main/java/com/mk/madpractical/MainActivity.java
@@ -0,0 +1,85 @@
+package com.mk.madpractical;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Handler;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView l;
+    String[] tutorials = {
+            "About: Author | Source code",
+            "Practical-01: Setup Android Studio & Hello World",
+            "Practical-02: Activity Lifecycle Demo App",
+            "Practical-03: Basic Calculator App",
+            "Practical-04: LinearLayout UI Arrangement",
+            "Practical-05: RelativeLayout UI Arrangement",
+            "Practical-06: ScrollView for Long Lists",
+            "Practical-07: ListView with Custom Adapter",
+            "Practical-08: Navigation Drawer Implementation",
+            "Practical-09: Bottom Navigation Tabs",
+            "Practical-10: Intent for Data Passing",
+            "Practical-11: Background Tasks with Services",
+            "Practical-12: Broadcast Receivers Usage",
+            "Practical-13: Data Sharing with Content Providers",
+            "Practical-14: Access System Data with Content Providers",
+            "Practical-15: SQLite DB: Insert & Read Data",
+            "Practical-16: SQLite DB: Update & Delete Data",
+            "Practical-17: Firebase Integration & Data Storage",
+            "Practical-18: Firebase Data Display in RecyclerView",
+            "Practical-19: MySQL Connection & Data Insertion",
+            "Practical-20: MySQL Data Insertion via PHP",
+            "Practical-21: Read MySQL Data via PHP & JSON",
+            "Practical-22: Google Maps API: Display Location",
+            "Practical-23: Google Maps API: Distance Calculation",
+            "Practical-24: Google Account Login Integration"
+    };
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        l = findViewById(R.id.practicallist);
+        ArrayAdapter<String> arr;
+        arr = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, tutorials);
+        l.setAdapter(arr);
+
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i){
+                    case 0:
+                        Intent about = new Intent(MainActivity.this,about.class);
+                        startActivity(about);
+                        break;
+                    case 1:
+                        Intent pr1 = new Intent(MainActivity.this,Practical1.class);
+                        startActivity(pr1);
+                        break;
+                    case 2:
+                        Intent pr2 = new Intent(MainActivity.this,Practical2.class);
+                        startActivity(pr2);
+                        break;
+                    case 3:
+                        Intent pr3 = new Intent(MainActivity.this,Practical2.class);
+                        startActivity(pr3);
+                        break;
+
+                    default:
+                        Toast.makeText(MainActivity.this, "Practical: "+i+" is comming soon", Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/mk/madpractical/Practical1.java
@@ -0,0 +1,26 @@
+package com.mk.madpractical;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Practical1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_practical1);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        Toast.makeText(this, "welcome to Practical 1", Toast.LENGTH_SHORT).show();
+    }
+}
